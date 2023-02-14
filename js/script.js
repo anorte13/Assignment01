b00dd177a071c5c24a4a6f1a57a994ecc50b0f1b
Angel <angel-ortega27@hotmail.com>
2023-02-13 16:56:36 -0800

    Completed all questions and removed all comments

---
+++ js/script.js
@@ -0,0 +1,69 @@
+let someMonth;
+function theMonth() {}
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+
+let number = 1;
+let string = "Hello";
+let value = false;
+let value2 = null;
+
+let total = number + value;
+const complicatedExpression = theMonth();
+
+let strfirstName;
+let strlastName;
+let strAddrses;
+let strCity;
+let strState;
+let intZipcode;
+//let intAge;
+let txtreferralSource;
+let txtmayWeContactYou;
+
+strFirstName = "Angel";
+(txtmayWeContactYou = true), (txtreferralSource = false);
+let intAge = "27";
+
+let birthday = "January " + 25;
+console.log(birthday);
+
+let isStudent = "2" + true;
+console.log(isStudent);
+let units = 12 + false;
+console.log(units);
+
+let someString =
+  'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+
+let someVal = false;
+console.log(someVal);
+
+console.log(typeof string, typeof units, typeof someVal, typeof strfirstName);
+
+const name = prompt("What is your name?");
+let course = prompt("What class are you taking?");
+window.alert("Hello " + name + ".\n" + "Welcome to the " + course + " class!");
+
+let x = 10;
+let y = 20;
+console.log(x + y);
+
+let x2 = 20 + 20;
+console.log(x2);
+
+let x3 = 20 * 5;
+console.log(x3);
+
+let x4 = 20 % 3;
+console.log(x4);
+
+let t = 5;
+let u = 10;
+console.log(t > u);
+
+let h = "Hello";
+let q = "Hello";
+console.log(h === q);
